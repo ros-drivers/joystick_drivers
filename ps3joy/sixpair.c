6ce04feb83f5f36d9cd84aae64e15441c1640f64
blaisegassend <blaisegassend@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-09-23 17:25:22 +0000

    Updated license
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/joystick_drivers/trunk@24366 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- ps3joy/sixpair.c
+++ ps3joy/sixpair.c
@@ -1,6 +1,30 @@
 /*
- * sixpair.c version 2007-04-18
- * Compile with: gcc -o sixpair sixpair.c -lusb
+ * Copyright (c) 2007, 2008 pascal@pabr.org
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <string.h>
@@ -42,7 +66,7 @@ void set_master(usb_dev_handle *devh, int itfnum, int mac[6]) {
 
 void process_device(int argc, char **argv, struct usb_device *dev,
 		    struct usb_config_descriptor *cfg, int itfnum) {
-  int mac[6], have_mac=0;
+  int mac[6];
 
   usb_dev_handle *devh = usb_open(dev);
   if ( ! devh ) fatal("usb_open");
