24042aceab14b4f5f081d1e3775a86a2e3945a53
Christian Eichmann <eichmann@fzi.de>
2022-02-15 10:36:47 +0100

    Changed name of executable in launch files to match installed node
    Signed-off-by: Christian Eichmann <eichmann@fzi.de>

--- spacenav/launch/no_deadband-launch.py
+++ spacenav/launch/no_deadband-launch.py
@@ -30,10 +30,21 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(package='spacenav', executable='spacenav',
-             name='spacenav', namespace='', output='screen',
-             parameters=[{'zero_when_static': False,
-                          'static_trans_deadband': 0.0,
-                          'static_rot_deadband': 0.0}]),
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package='spacenav',
+                executable='spacenav_node',
+                name='spacenav',
+                namespace='',
+                output='screen',
+                parameters=[
+                    {
+                        'zero_when_static': False,
+                        'static_trans_deadband': 0.0,
+                        'static_rot_deadband': 0.0,
+                    }
+                ],
+            ),
+        ]
+    )
